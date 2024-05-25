6453439c82c1408db79e3c76a2b71a0dcd828fee
Wojciech Jozwik <wj.telefon@gmail.com>
2024-05-26 01:11:23 +0200

    Add redux

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import initialState from './initialState';
+import tablesReducer from './tablesRedux';
+
+const subreducers = {
+  tables: tablesReducer,
+};
+
+const reducer = combineReducers(subreducers);
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
